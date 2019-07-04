62f49580a7648ec0dbe3b3c6977d8b95b6ae3e4d
zhouzq-thu <zzq_890709@hotmail.com>
2019-07-04 08:06:48 +0800

    Initial Commit

---
+++ HOGImage/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HOGImage
+//
+//  Created by Zhi-Qiang Zhou on 7/4/19.
+//  Copyright © 2019 Zhi-Qiang Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
